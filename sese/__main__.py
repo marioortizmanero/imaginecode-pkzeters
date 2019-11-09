f7c186e6e3153aace49880f4eda8a045db9277ed
tomenos <tomenos@C.Rex>
2019-11-09 02:49:22 +0100

    Tareas en progreso

--- sese/__main__.py
+++ sese/__main__.py
@@ -1,16 +1,30 @@
-from sese.contenedor import Contenedor
+from sese.contenedor import Almacen, Tarea, NoHayItems
 
 
 ARCHIVO_ALMACEN = 'almacen.json'
 ARCHIVO_FINAL = 'final.json'
 ARCHIVO_TAREAS = 'tareas.json'
+ARCHIVO_PEDIDOS = 'pedidos.json'
 
 
 def main():
-    contenedor = Contenedor()
-    contenedor.leer(ARCHIVO_ALMACEN)
-    for item in contenedor:
-        print(item)
+    almacen = Almacen()
+    almacen.leer(ARCHIVO_ALMACEN)
+
+    pedidos = Almacen()
+    pedidos.leer(ARCHIVO_PEDIDOS)
+
+    tareas = []
+    for pedido in pedidos:
+        try:
+            tareas.append(Tarea(almacen, pedido))
+        except NoHayItems as e:
+            print(e)
+
+    for estanteria in almacen:
+        for item in estanteria:
+            print(item)
+        print("---")
 
 
 if __name__ == '__main__':
--- sese/contenedor.py
+++ sese/contenedor.py
@@ -1,7 +1,8 @@
+from typing import Tuple
 import json
 
 
-class Contenedor:
+class Almacen:
     def leer(self, archivo: str) -> None:
         # Guarda los datos del JSON en el objeto
         with open(archivo, 'r') as archivo:
@@ -10,8 +11,10 @@ class Contenedor:
         # Asignar los items de cada estanteria del contenedor
         self.estanterias = []
         for estanteria in self.items['items']:
+            items = []
             for nombre, cantidad in estanteria.items():
-                self.estanterias.append(Item(nombre, cantidad))
+                items.append(Item(nombre, cantidad))
+            self.estanterias.append(items)
 
     def __iter__(self):
         # Para iterar cada estanteria del contenedor
@@ -28,6 +31,34 @@ class Item:
         return f'{self.nombre}: {self.cantidad}'
 
 
+class NoHayItems(Exception):
+    def __init__(self, msg: str) -> None:
+        super().__init__(self, msg)
+
+
 class Tarea:
-    def __init__(self):
-        pass
+    def __init__(self, almacen: Almacen, pedido: Tuple[Item]):
+        if not self.se_puede(almacen, pedido):
+            raise NoHayItems('No se puede hacer el pedido: {pedido}')
+
+        # Itera cada item del pedido
+        for item_pedido in pedido:
+            # Los busca en todos los items del almacén
+            for estanteria in almacen:
+                for item_disponible in estanteria:
+                    # Se mueven los items del almacén requeridos a los del
+                    # pedido.
+                    if item_disponible.nombre == item.nombre:
+
+
+    def se_puede(self, almacen: Almacen, pedido: Tuple[Item]):
+        for item in pedido:
+            contador = 0
+            for estanteria in almacen:
+                for item_disponible in estanteria:
+                    if item_disponible.nombre == item.nombre:
+                        contador += item_disponible.cantidad
+            if contador < item.cantidad:
+                return False
+
+        return True
